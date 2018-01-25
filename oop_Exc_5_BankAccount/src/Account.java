d68becbd9856aa18f06e21ad792543fa2dfdd6ac
Unknown <etienne.langensiepen@gmx.de>
2018-01-25 16:32:47 +0100

    BankAccount_AccountClass

--- oop_Exc_5_BankAccount/src/Account.java
+++ oop_Exc_5_BankAccount/src/Account.java
@@ -1,20 +1,46 @@
 public class Account {
 
-    private double balance = 100;
+    private double balance;
     private String accountNumber;
 
-    public void deposit() {
+    public Account(int bal, String acc) {
 
+        if (bal > 100) {
+            balance = bal;
+            System.out.println("New balance: " + balance);
+        }
+        else
+            System.out.println("Initial balance has to be at least 100.");
 
+        accountNumber = acc;
     }
 
-    public void withdraw() {
+    public void deposit(double depo) {
 
-
+        if (depo > 0) {
+            balance += depo;
+            System.out.println(depo + " has been added to balance." +
+            "\n New balance: " + balance);
+        }
+        else
+            System.out.println("Please ensure the amount to be deposited is not negative.");
     }
 
-    public void getBalance() {
+    public void withdraw(double withdr) {
 
+        if (withdr > 0 && withdr < balance) {
+            balance -= withdr;
+            System.out.println(withdr + " has been successfully withdrawn." +
+            "\nNew balance: " + balance);
+        }
+        else if (withdr < 0)
+            System.out.println("Please ensure the amount to be withdrawn is not negative.");
+        else if (withdr > balance)
+            System.out.println("Amount to be withdrawn is higher than your balance.");
+    }
 
+    public String getAccDetails() {
+
+        return "Account: " + accountNumber + "\nBalance: " + balance;
     }
 }
