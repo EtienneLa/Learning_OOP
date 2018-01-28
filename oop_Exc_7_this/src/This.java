3ea038e7b70e46b081be996f42c7b5efeccc5508
Unknown <etienne.langensiepen@gmx.de>
2018-01-28 15:16:52 +0100

    oop_Exc_7_this

---
+++ oop_Exc_7_this/src/This.java
@@ -0,0 +1,26 @@
+public class This {
+
+    public static void main(String[] args) {
+
+        Test test = new Test();
+        test.display();
+        System.out.println("test = " + test);
+
+        test.num = 20;
+        test.display();
+        
+    }
+}
+
+class Test {
+
+    int num = 1;
+
+    public void display() {
+
+        int num = 5;
+        System.out.println(num);
+        System.out.println(this.num);
+        System.out.println("this keyword = "+ this);
+    }
+}
