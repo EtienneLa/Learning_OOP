d983a95151e68afedf085b1cb571d07ba935ac45
Unknown <etienne.langensiepen@gmx.de>
2018-02-02 22:34:53 +0100

    oop_Exc_9_inheritance

---
+++ oop_Exc_9_inheritance/src/Oop_Inheritance.java
@@ -0,0 +1,38 @@
+public class Oop_Inheritance {
+
+    public static void main(String[] args) {
+
+        StudentSpeaker studSpeaker = new StudentSpeaker("Max Mustermann", 16, 79.6, 11);
+
+        studSpeaker.getInfo();
+    }
+}
+
+class Person {
+
+    String name;
+    int age;
+}
+
+class Pupil extends Person {
+
+    double gradeAverage;
+}
+
+class StudentSpeaker extends Pupil {
+
+    int academicYear;
+
+    StudentSpeaker(String name, int age, double gradeAverage, int academicYear) {
+        this.name = name;
+        this.age = age;
+        this.gradeAverage = gradeAverage;
+        this.academicYear = academicYear;
+    }
+
+    void getInfo() {
+
+        System.out.println(this.name + ", " + this.age + " years old, " + this.gradeAverage + " avrg, " + this.academicYear + "th grade.");
+    }
+}
+
